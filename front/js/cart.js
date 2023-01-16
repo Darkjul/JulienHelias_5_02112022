a822bfe54526c13eed35a5cbbbdbbc002047ca99
Julien Helias <julien.helias@free.fr>
2023-01-16 16:45:34 +0100

    Modif fonction deleteArticle et refactoring final

--- front/js/cart.js
+++ front/js/cart.js
@@ -207,9 +207,18 @@ function deleteArticle(dataSofaId, dataSofaColor) {
 
     let reloadCart = cartFilter;
 
-    // On sauvegarde la modification dans le Local Storage
+    if (cartFilter.length > 0) {
 
-    localStorage.setItem('sofa', JSON.stringify(reloadCart));
+        localStorage.setItem('sofa', JSON.stringify(reloadCart));
+
+    }
+
+    // Si le panier est vide on purge le Local Storage et retour à la page d'acceuil
+
+    else {
+
+        localStorage.removeItem('sofa');
+    }
 
     // On recharge la page panier avec les modifications 
 
